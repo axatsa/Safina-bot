45f012c985bea606af401918988a3665cc3998a1
axatsa <salamovakhat@gmail.com>
2026-04-11 02:22:30 +0500

    Fix NameError in projects.py

--- backend/app/api/projects.py
+++ backend/app/api/projects.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
-from typing import List
+from typing import List, Optional
 import os
 from app.db import models, schemas, crud
 from app.core import auth, database
